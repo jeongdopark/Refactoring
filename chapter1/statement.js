f9e6fde7e1bd64a7f5533b84e1e1396c58de3c14
jeongdopark <jdpark0816@gmail.com>
2021-07-26 23:47:49 +0900

    refactor : thisAmount 변수 인라인

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -14,17 +14,14 @@ function statement(invoices, plays){
                         minimumFractionDigits : 2}).format;
     
     for(let perf of invoices.performances){
-        // const play = playFor(perf); --> playFor(perf) 인라인된 변수는 제거 ! 
-        
-        // let thisAmount = amountFor(perf, playFor(perf)); //필요없는 매개변수 제거
-        let thisAmount = amountFor(perf);
+
 
         volumeCredit += Math.max(perf.audience - 30, 0);
 
         if("comedy" === playFor(perf).type) volumeCredit += Math.floor(perf.audience / 5);
 
-        result += ` ${playFor(perf).name} : ${format(thisAmount/100)} (${perf.audience}석\n)`;
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).name} : ${format(amountFor(perf)/100)} (${perf.audience}석\n)`;
+        totalAmount += amountFor(perf);
     }
     result += `총액 : ${format(totalAmount / 100)}\n`;
     result += `적립 포인트 : ${volumeCredit}점\n`;
@@ -32,32 +29,5 @@ function statement(invoices, plays){
 }
 
 
-// function playFor(aPerformance){
-//     return plays[aPerformance.playID];
-// }
-
-// function amountFor(aPerformance){   // 필요 없어진 Play 매개변수 제거
-
-//     let result = 0;     // 함수에서 return하게 될 값을 result 변수로 지정
-
-//     switch(playFor(aPerformance).type){
-//         case "tragedy":
-//             result = 40000;
-//             if(aPerformance.audience > 30){
-//                 result += 1000 * (aPerformance.audience - 30);
-//             }
-//             break;
-//         case "comedy":
-//             result = 30000;
-//             if(aPerformance.audience > 20){
-//                 result += 10000 + 500 * (aPerformance.audience - 20);
-//             }
-//             result += 300 * aPerformance.audience;
-//             break;
-//         default:
-//             throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type.type}`);
-//     }
-//     return result;
-// }
 
 console.log(statement(invoices, plays));
