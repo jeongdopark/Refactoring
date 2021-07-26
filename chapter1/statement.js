9b78c022767778406baa7c656e64a0dfeb51e593
jeongdopark <jdpark0816@gmail.com>
2021-07-26 19:07:46 +0900

    feat : statement함수 내부 switch문함수로 추출하기

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -13,24 +13,28 @@ function statement(invoices, plays){
     
     for(let perf of invoices.performances){
         const play = plays[perf.playID];
-        let thisAmount = 0;
+        let thisAmount = amountFor(perf, play);
+        function amountFor(perf, play){
+            let thisAmount = 0;
 
-        switch(play.type){
-            case "tragedy":
-                thisAmount = 40000;
-                if(perf.audience > 30){
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if(perf.audience > 20){
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르 : ${play.type}`);
+            switch(play.type){
+                case "tragedy":
+                    thisAmount = 40000;
+                    if(perf.audience > 30){
+                        thisAmount += 1000 * (perf.audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    thisAmount = 30000;
+                    if(perf.audience > 20){
+                        thisAmount += 10000 + 500 * (perf.audience - 20);
+                    }
+                    thisAmount += 300 * perf.audience;
+                    break;
+                default:
+                    throw new Error(`알 수 없는 장르 : ${play.type}`);
+            }
+            return thisAmount;
         }
         volumeCredit += Math.max(perf.audience - 30, 0);
 
