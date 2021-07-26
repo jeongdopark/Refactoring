e728322c3c4506431e240b5d3f9234e82a8f7c5a
jeongdopark <jdpark0816@gmail.com>
2021-07-27 00:14:34 +0900

    refactor :volumeCreditCalculator 모듈화

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -3,7 +3,7 @@
 import {invoices, plays} from './variable.js'
 import {amountFor} from './amountFor.js'
 import {playFor} from './playFor.js'
-
+import {volumeCreditCalculate} from './volumeCreditCalculate.js'
 
 function statement(invoices, plays){
     let totalAmount = 0;
@@ -11,23 +11,18 @@ function statement(invoices, plays){
     let result = `청구 내역 (고객명 : ${invoices.customer})\n`;
     const format = new Intl.NumberFormat("en-US",
                             {style : "currency", currency: "USD",
-                        minimumFractionDigits : 2}).format;
+                            minimumFractionDigits : 2}).format;
     
     for(let perf of invoices.performances){
-
-
-        volumeCredit += Math.max(perf.audience - 30, 0);
-
-        if("comedy" === playFor(perf).type) volumeCredit += Math.floor(perf.audience / 5);
+        volumeCredit += volumeCreditCalculate(perf)
 
         result += ` ${playFor(perf).name} : ${format(amountFor(perf)/100)} (${perf.audience}석\n)`;
         totalAmount += amountFor(perf);
     }
+
     result += `총액 : ${format(totalAmount / 100)}\n`;
     result += `적립 포인트 : ${volumeCredit}점\n`;
     return result;
 }
-
-
 
 console.log(statement(invoices, plays));
