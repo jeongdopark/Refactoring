ccc986674ce4946735130598a9adc2a2e8c50d0e
jeongdopark <jdpark0816@gmail.com>
2021-07-27 00:29:00 +0900

    refactor : format 모듈화

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -4,14 +4,12 @@ import {invoices, plays} from './variable.js'
 import {amountFor} from './amountFor.js'
 import {playFor} from './playFor.js'
 import {volumeCreditCalculate} from './volumeCreditCalculate.js'
-
+import {format} from './format.js'
 function statement(invoices, plays){
     let totalAmount = 0;
     let volumeCredit = 0;
     let result = `청구 내역 (고객명 : ${invoices.customer})\n`;
-    const format = new Intl.NumberFormat("en-US",
-                            {style : "currency", currency: "USD",
-                            minimumFractionDigits : 2}).format;
+    
     
     for(let perf of invoices.performances){
         volumeCredit += volumeCreditCalculate(perf)
@@ -24,5 +22,7 @@ function statement(invoices, plays){
     result += `적립 포인트 : ${volumeCredit}점\n`;
     return result;
 }
+
+
 
 console.log(statement(invoices, plays));
